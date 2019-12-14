9d90cdc25a5fb2ed7504640c7b4596c60118264b
atlas-quest <vanessa@sveltestudios.com>
2019-12-15 01:47:58 +1030

    css mobile menu

--- components/nav/Mobile.vue
+++ components/nav/Mobile.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="fixed inset-0 p-8 bg-secondary z-30">
-  <div class="flex items-center">
+<div class="fixed inset-0 bg-secondary z-30">
+  <div class="flex items-center p-8">
     <div class="w-1/2 justify-start items-center">
       <nuxt-link to="/">
         <i-logo class="text-accent h-12"></i-logo>
@@ -10,6 +10,25 @@
          @click="$emit('close')">
       <i-close class="h-8 text-accent"></i-close>
     </div>
+  </div>
+  <div class="flex flex-col w-full text-center">
+    <div class="border-b border-accent opacity-25"></div>
+    <h3 class="tracking-widest hover:bg-accent py-4 text-xl">
+      About
+    </h3>
+    <div class="border-b border-accent opacity-25"></div>
+    <h3 class="tracking-widest hover:bg-accent py-4 text-xl">
+      Testimonials
+    </h3>
+    <div class="border-b border-accent opacity-25"></div>
+    <h3 class="tracking-widest hover:bg-accent py-4 text-xl">
+      Projects
+    </h3>
+    <div class="border-b border-accent opacity-25"></div>
+    <h3 class="tracking-widest hover:bg-accent py-4 text-xl">
+      Skills
+    </h3>
+    <div class="border-b border-accent opacity-25"></div>
   </div>
 </div>
 </template>
