bb1b5be52f20bc6863f7902faa4a8005d0591b30
atlas-quest <vanessa@sveltestudios.com>
2019-12-15 01:21:29 +1030

    nav

--- components/nav/Mobile.vue
+++ components/nav/Mobile.vue
@@ -1,14 +1,25 @@
 <template>
-<div class="flex justify-end w-1/2"
-     @click="$emit('close')">
-  <i-close class="h-8 text-accent"></i-close>
+<div class="fixed inset-0 p-8 bg-secondary z-30">
+  <div class="flex items-center">
+    <div class="w-1/2 justify-start items-center">
+      <nuxt-link to="/">
+        <i-logo class="text-accent h-12"></i-logo>
+      </nuxt-link>
+    </div>
+    <div class="flex justify-end w-1/2"
+         @click="$emit('close')">
+      <i-close class="h-8 text-accent"></i-close>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
 import iClose from '~/components/icons/Close'
+import iLogo from '~/components/icons/Logo'
+
 export default {
   name: 'MobileNav',
-  components: { iClose }
+  components: { iClose, iLogo }
 }
 </script>
