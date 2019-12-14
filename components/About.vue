0fa23bd016c1b7a4816e89cb1a9c3c79dd58a13e
atlas-quest <vanessa@sveltestudios.com>
2019-12-14 20:48:54 +1030

    about me section

--- components/About.vue
+++ components/About.vue
@@ -1,25 +1,26 @@
 <template>
-  <div>
-    <div class="p-8">
-      <div class="w-full pb-4 md:w-1/2">
-        <h2 class="title text-center text-accent pb-2">
-          About
-        </h2>
-        <div class="flex items-center">
-          <div class="d-inline avatar bg-accent w-4 h-4"></div>
-          <div class="mx-2 d-inline text-gray-700">Matt Martin</div>
-        </div>
-      </div>
-      <p>
-        Hi, nice to meet you. I am a full stack web developer with two years
-        experience in a fast paced startup company. etc.
-      </p>
+<div>
+  <div class="p-8">
+    <div class="w-full pb-4 md:w-1/2">
+      <h2 class="title text-center text-accent pb-2">
+        About
+      </h2>
     </div>
-    <div
-      class="hero-image w-full h-64 md:w-1/2"
-      style="background-image: url('./computer.jpg')"
-    ></div>
+    <p>
+      I write clean, maintainable code. I enjoy solving complex problems and playing guitar.
+    </p>
+    <br>
+    <p>
+      I'm currently leading a team of developers on two projects.
+    </p>
+    <br>
+    <p>
+      All with different levels of skill on projects pushing the boundaries of tech in the Mobile Apps space such as Nativescipt.
+    </p>
   </div>
+  <div class="hero-image w-full h-64 md:w-1/2"
+       style="background-image: url('./computer.jpg')"></div>
+</div>
 </template>
 
 <script>
